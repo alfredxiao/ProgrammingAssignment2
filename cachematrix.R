694f22691eb7984815f9416255be18a423a9b869
Alfred Xiao <alfred.xiao@oracle.com>
2014-12-15 21:51:45 +1100

    [Alfred] initial import of cache matrix which caches result of solving an invertible matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Overall Comments: cache result of solving a matrix and returns the result when cache found
+## Example usage:
+## > m1 <- rbind(c(1, -1/4), c(-1/4, 1))
+## > class(m1)
+## [1] "matrix"
+## > m2 <- makeCacheMatrix(m1)
+## > class(m2)
+## [1] "list"
+## > cacheSolve(m2)
+##           [,1]      [,2]
+## [1,] 1.0666667 0.2666667
+## [2,] 0.2666667 1.0666667
+## > cacheSolve(m2)
+## getting cached data
+##           [,1]      [,2]
+## [1,] 1.0666667 0.2666667
+## [2,] 0.2666667 1.0666667
 
-## Write a short comment describing this function
+## makeCacheMatrix converts an matrix object into an object containing the cached value from potentially costly computation
 
 makeCacheMatrix <- function(x = matrix()) {
+        s <- NULL
+        set <- function(y) {
+                x <<- y
+                s <<- NULL
+        }
+        get <- function() x
+        set_solved <- function(solved) s <<- solved
+        get_solved <- function() s
+        list(set = set, get = get,
+             set_solved = set_solved,
+             get_solved = get_solved)
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve accepts an object made via makeCacheMatrix() and returns calculated or cached result of solve
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        s <- x$get_solved()
+        if(!is.null(s)) {
+                message("getting cached data")
+                return(s)
+        }
+        data <- x$get()
+        s <- solve(data, ...)
+        x$set_solved(s)
+        s
 }
